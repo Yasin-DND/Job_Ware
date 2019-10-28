e6481fca76f78e931161f8c2ed21f032781d42e4
Yasin <yasinmemon482@yahoo.com>
2019-10-28 19:51:36 +0500

    Initial Commit

---
+++ Job_Ware/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Job_Ware
+//
+//  Created by Mac on 28/10/2019.
+//  Copyright © 2019 DND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
